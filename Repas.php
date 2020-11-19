9045d829280108cbf319ae30e0f72e2f99d31525
Toto97420 <pizzapaulo420@gmail.com>
2020-11-19 14:08:57 +0400

    ajout Repas.php

---
+++ Repas.php
@@ -0,0 +1,110 @@
+<?php
+    class Repas {
+        private $id ;
+        private $nom;
+        private $description;
+        private $photo;
+        private $prix;
+
+        
+
+        /**
+         * Get the value of id
+         */ 
+        public function getId()
+        {
+                return $this->id;
+        }
+
+        /**
+         * Set the value of id
+         *
+         * @return  self
+         */ 
+        public function setId($id)
+        {
+                $this->id = $id;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of nom
+         */ 
+        public function getNom()
+        {
+                return $this->nom;
+        }
+
+        /**
+         * Set the value of nom
+         *
+         * @return  self
+         */ 
+        public function setNom($nom)
+        {
+                $this->nom = $nom;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of description
+         */ 
+        public function getDescription()
+        {
+                return $this->description;
+        }
+
+        /**
+         * Set the value of description
+         *
+         * @return  self
+         */ 
+        public function setDescription($description)
+        {
+                $this->description = $description;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of prix
+         */ 
+        public function getPrix()
+        {
+                return $this->prix;
+        }
+
+        /**
+         * Set the value of prix
+         *
+         * @return  self
+         */ 
+        public function setPrix($prix)
+        {
+                $this->prix = $prix;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of photo
+         */ 
+        public function getPhoto()
+        {
+                return $this->photo;
+        }
+
+        /**
+         * Set the value of photo
+         *
+         * @return  self
+         */ 
+        public function setPhoto($photo)
+        {
+                $this->photo = $photo;
+
+                return $this;
+        }
+    }
