613a3b6623b4b39c8742e03972a66cf5be931655
Toto97420 <pizzapaulo420@gmail.com>
2020-11-19 13:28:00 +0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # projet_repas
-Réalisation d'un site permettant à un utilisateur de s'y connecter et de commander ces repas. ( PHP/PDO)
+Réalisation d'un site permettant à un utilisateur de s'y connecter et de commander ces repas. En compagnie de Julien( PHP/PDO)
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+            <title>Le bon barquette</title>
+    </head>
+    <body>
+
+        <div class="form-group">
+            <label for="nom">Nom du repas</label>
+            <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+        
+        </div>
+        <div class="form-group">
+            <label for="nom">Description du repas</label>
+            <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+       
+        </div>
+        <div class="form-group">
+            <label for="nom">Photo du repas</label>
+            <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+        
+        </div>
+        <div class="form-group">
+            <label for="nom">Prix</label>
+            <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+     
+        </div>
+
+        </body>
+</html>
